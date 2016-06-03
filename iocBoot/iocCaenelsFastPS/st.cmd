c9802be8b7cda77c40c1e9ceeb5cd4db2785f3e4
Jan Marjanovic <j.marjanovic@caenels.com>
2016-06-03 17:48:40 +0200

    Added streamDevice protocol file with basic commands

--- iocBoot/iocCaenelsFastPS/st.cmd
+++ iocBoot/iocCaenelsFastPS/st.cmd
@@ -1,18 +1,22 @@
-#!../../bin/linux-x86/CaenelsFastPS
+#!../../bin/linux-x86_64/CaenelsFastPS
 
-## You may have to change CaenelsFastPS to something else
-## everywhere it appears in this file
+< envPaths
 
-#< envPaths
+epicsEnvSet ("STREAM_PROTOCOL_PATH", ".:../../protocols")
+epicsEnvSet("P","$(R=caenels:)")
+epicsEnvSet("R","$(R=fastps:)")
+
+cd "${TOP}"
 
 ## Register all support components
-dbLoadDatabase("../../dbd/CaenelsFastPS.dbd",0,0)
-CaenelsFastPS_registerRecordDeviceDriver(pdbbase) 
+dbLoadDatabase("dbd/CaenelsFastPS.dbd",0,0)
+CaenelsFastPS_registerRecordDeviceDriver(pdbbase)
+
+## Configure device
+drvAsynIPPortConfigure("L0","192.168.0.10:10001",0,0,0)
 
 ## Load record instances
-dbLoadRecords("../../db/CaenelsFastPS.db","user=jan")
+dbLoadRecords("db/fastps.db", "P=$(P),R=$(R)")
 
+cd "${TOP}/iocBoot/${IOC}"
 iocInit()
-
-## Start any sequence programs
-#seq sncCaenelsFastPS,"user=jan"
